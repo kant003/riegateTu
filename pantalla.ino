7282998f16ff51798a443ce87ab188de96fb6752
Yari <yariana894@gmail.com>
2021-05-24 11:50:51 +0200

    subo codigo pantalla

--- pantalla.ino
+++ pantalla.ino
@@ -4,27 +4,22 @@
  */
  
 #include <DHT.h>
-#include <DHT.h>
-#include <LiquidCrystal_I2C.h>
-LiquidCrystal_I2C lcd(0x27, 16, 2);
+#include <LiquidCrystal.h>
 
-int sensor = 2;
-int temp, humedad;
-int boton = 3;
-const int nivel = 9;
-const int humedadSuelo = A0;
+int SENSOR = 2;
+DHT dht(SENSOR, DHT11);
+LiquidCrystal lcd(4,5,6,7,8,9);
 
-DHT dht(sensor, DHT11);
-int botonest = 0;
+int temp;
+int humedad;
 
 void setup() {
  Serial.begin(9600);
  dht.begin();
+ lcd.begin(16, 2);
+ analogWrite(V0, 50)
 
  pinMode(humedadSuelo, INPUT);
- pinMode(bomba, OUTPUT);
- pinMode(nivel, INPUT);
- pinMode(boton, INPUT);
 
  lcd.init();
  lcd.backlight();
@@ -33,28 +28,22 @@ void setup() {
 }
 
 void loop() {
- 
-  int SensorNivel = digitalRead(nivel);
-  int botonest = digitalRead(boton);
+  int SensorValue = analogRead(humedadSuelo);
 
+  humedad = dht.readHumidity();
   temp = dht.readTemperature();
 
-  void temperatura(){
-     Serial.print("Temperatura: ");
-     Serial.print(temp);
-  }
+  lcd.clear();
 
-  void humedad(){
-    Serial.print("ºC Humedad: ");
-    Serial.print(humedad);
-    Serial.println("%");
-  }
+  Serial.print("Temperatura: ");
+  Serial.print(temp);
+  Serial.println("ºC"); 
 
-  void humedadSuelo(){
-    int SensorValue = analogRead(humedadSuelo);
-    Serial.print("Humedad del suelo: ");
-    Serial.print(SensorValue);
-    Serial.println("%");
-  }
+  Serial.print("Humedad: ");
+  Serial.print(humedad);
+  Serial.println("%");
+
+  Serial.print("Humedad del suelo: ");
+  Serial.print(SensorValue);
+  Serial.println("%");
   delay(500);
-}
