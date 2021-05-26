3c376fd5ac1fddeb9973df44e8c81cb604adfbfb
unknown <alexandrerapososocasas@gmail.com>
2021-05-26 12:51:52 +0200

    codigo del sensor de humedad de la tierra

--- sensorHumedadTierra.ino
+++ sensorHumedadTierra.ino
@@ -1,12 +1,16 @@
 const int sensorPin = A0;
-int getHumedadSuelo;
+
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
- 
-  getHumedadSuelo = analogRead(sensorPin);
-  return getHumedadSuelo;
- 
+   
+
+}
+int getHumedadSuelo(){
+   
+  int humedadSuelo = analogRead(sensorPin);
+  return humedadSuelo;
+
 }
